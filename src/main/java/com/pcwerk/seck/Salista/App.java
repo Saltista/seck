e24cfec8f958bcddae36b99ef9e2a51b9efed76f
Salvador Bautista <squall20@sbcglobal.net>
2012-09-29 14:05:39 -0700

    first commit

---
+++ src/main/java/com/pcwerk/seck/Salista/App.java
@@ -0,0 +1,13 @@
+package com.pcwerk.seck.Salista;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sal Bautista says Hello World!");
+	}
+
+}
